ffaeb8ccc7dc74b611dc527d7ba05e7dc663a9f9
TheBestByte <dom_jackson_92@hotmail.com>
2023-04-05 18:14:12 +1000

    models created using mongoose

---
+++ models/Thought.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require('mongoose');
+
+const reactionSchema = new Schema({
+  reactionBody: { type: String, required: true, maxLength: 280 },
+  username: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => new Date(timestamp).toLocaleString(),
+    },
+    username: { type: String, required: true },
+    reactions: [reactionSchema],
+  },
+  { toJSON: { virtuals: true }, id: false }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
